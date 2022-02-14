19be8d4ccd0bf4b493e10ea577fbbab54ea687c9
mohmmad-owais <precisework01@gmail.com>
2022-02-14 15:59:03 +0500

    change folder structure

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,34 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 const app = express();
 
+dotenv.config();
 
-  dotenv.config();
-
-  mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => console.log("DBconnection sucessfull"))
-    .catch((err) => {
-      console.log(err);
-    });
-
-  app.get("/api/test", () => {
-    console.log("test is sucessfull");
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DBconnection sucessfull"))
+  .catch((err) => {
+    console.log(err);
   });
 
-  app.use(express.json());
-  app.use("/api/auth", authRoute);
-  app.use("/api/users", userRoute);
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
-  app.listen(5000, () => {
-    console.log("Backend is running");
-  });
+app.listen(5000, () => {
+  console.log("Backend is running");
+});
